3edc1523fd1bc30380c30902b875fa9e466a42b5
Mary West <westmarye1219@gmail.com>
2018-09-29 11:38:34 -0500

    added stuff

---
+++ javascripts/components/calculator.js
@@ -0,0 +1,25 @@
+import {printToDom} from '../helpers/util.js';
+import {multiply} from '../helpers/maths.js';
+
+const calculate = (num1, num2, mathType) => {
+    let answer = 0;
+    switch(mathType) {
+        case 'multiply':
+            answer = multiply(num1, num2)
+            break;
+        case 'divide':
+            answer = num1 / num2
+            break;
+        case 'add':
+            answer = num1 + num2;
+            break;
+        case 'subtract':
+            answer = num1-num2
+            break;
+        default:
+            answer ='nope'; 
+    }
+    printToDom(answer, 'result');
+};
+
+export {calculate};
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,26 +1,3 @@
-
-import {printToDom} from './helpers/util.js';
-import {multiply} from './helpers/maths.js';
-
-const calculate = (num1, num2, mathType) => {
-    let answer = 0;
-    switch(mathType) {
-        case 'multiply':
-            answer = multiply(num1, num2)
-            break;
-        case 'divide':
-            answer = num1 / num2
-            break;
-        case 'add':
-            answer = num1 + num2;
-            break;
-        case 'subtract':
-            answer = num1-num2
-            break;
-        default:
-            answer ='nope'; 
-    }
-    printToDom(answer, 'result');
-};
+import{calculate} from './components/calculator.js';
 
 calculate(4, 2, 'multiply');
